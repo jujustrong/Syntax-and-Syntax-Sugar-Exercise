575f360468a0c76978c579e9a2fd0a6f6ef2dbf0
Julian Armstrong <julian.armstrong8@gmail.com>
2024-08-12 23:00:31 -0600

    Completed Exercise 1

--- SyntaxAndSyntaxSugarExercise/Program.cs
+++ SyntaxAndSyntaxSugarExercise/Program.cs
@@ -4,7 +4,15 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            LessThanNine();
+        }
+
+        public static void LessThanNine()
+        {
+            Random n = new Random();
+            var answer = n.Next(1, 100);
+            var response = (answer < 9) ? $"{answer} is less than nine" : $"{answer} is greater than or equal to nine";
+            Console.WriteLine(response);
         }
     }
 }
